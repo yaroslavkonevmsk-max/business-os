83aa722f31434ae950c7b0cf17fe16af08032c93
yaroslavkonevmsk-max <yaroslavkonevmsk@gmail.com>
2026-07-13 21:25:58 +0300

    Update start.bat to run the smart PowerShell launcher

--- start.bat
+++ start.bat
@@ -1,11 +1,7 @@
 @echo off
-cd /d C:\KonCoOS\business-os
-docker compose up --build -d
-echo.
-echo ============================================
-echo  Business OS запущен!
-echo  Backend API:  http://localhost:8000
-echo  Mini App:     http://localhost:3000
-echo  API Docs:     http://localhost:8000/docs
-echo ============================================
-pause
+:: Business OS - One-Click Launcher
+:: This file automatically runs the smart PowerShell launcher
+:: Just double-click this file!
+
+cd /d "%~dp0"
+powershell -ExecutionPolicy Bypass -File "start.ps1"
